65d48a15d0b6d7cd515aa528d8656c63abc4a18e
luyise <louise.leclerc37@gmail.com>
2021-06-23 17:56:04 +0200

    Progressed in the proof of linearity

--- simpl_fun_BInt.v
+++ simpl_fun_BInt.v
@@ -20,6 +20,7 @@ From MILC Require Import
 
 Require Import 
     simpl_fun
+    square_bij
 .
 
 Section BInt_for_sf.
@@ -41,8 +42,6 @@ Section BInt_for_sf.
         sum_n
             (fun n => scal (real (μ (nth_carrier sf n))) (sf.(val) n))
             (sf.(max_which)).
-    
-    Notation "'∫B' sf 'd' μ" := (BInt_sf (sf : @simpl_fun _ _ E _ μ)) (at level 10).
 
     (*
     Lemma BInt_sf_indep_of_dec :
@@ -75,6 +74,7 @@ Section Bint_sf_plus.
     Open Scope nat_scope.
 
     (*
+
     Lemma BInt_sf_plus_aux :
         ∀ sf_f sf_g : @simpl_fun _ _ E _ μ,
             BInt_sf (simpl_fun_plus_aux sf_f sf_g) = plus (BInt_sf sf_f) (BInt_sf sf_g).
@@ -89,8 +89,132 @@ Section Bint_sf_plus.
         replace (max_which (simpl_fun_plus_aux sf_f sf_g)) with ((S maxf) * (S maxg) - 1).
             2 : rewrite Eqf Eqg => //.
         unfold nth_carrier.
-    Abort.
+        rewrite square_bij_sum.
+        pose valfg := val (simpl_fun_plus_aux sf_f sf_g).
+            fold valfg.
+            assert (valfg = simpl_fun.val (simpl_fun_plus_aux sf_f sf_g))
+                as Eqval by unfold valfg => //.
+            clearbody valfg; rewrite Eqf Eqg in Eqval; simpl in Eqval.
+            rewrite Eqval; clear Eqval valfg.
+        pose whichfg := which (simpl_fun_plus_aux sf_f sf_g).
+            fold whichfg.
+            assert (whichfg = which (simpl_fun_plus_aux sf_f sf_g))
+                as Eqwhich by unfold whichfg => //.
+            clearbody whichfg; rewrite Eqf Eqg in Eqwhich; simpl in Eqwhich.
+            rewrite Eqwhich; clear Eqwhich whichfg.
+        assert 
+            (∀ k1 k2 : nat, k2 <= maxg ->
+                (μ
+                    (λ x : X,
+                        square_bij (S maxg) (wf x, wg x) =
+                        square_bij (S maxg) (k1, k2))
+                ) =
+                (μ
+                    (λ x : X,
+                        wf x = k1 ∧ wg x = k2
+                    )
+                )
+            ) as Eqμ.
+            move => k1 k2 Hk2.
+            apply measure_ext => x; split.
+                move => Eqwfk.
+                assert 
+                    (square_bij_inv (S maxg) 
+                        (square_bij (S maxg) (wf x, wg x))
+                    =
+                    square_bij_inv (S maxg) 
+                        (square_bij (S maxg) (k1, k2))
+                    ) as Eqaux by congruence.
+                rewrite is_bij_square in Eqaux.
+                    2 : apply axg2.
+                rewrite is_bij_square in Eqaux.
+                    2 : assumption.
+                split; congruence.
+                case => -> -> //.
+        assert 
+            (∀ k1 k2 : nat, k2 <= maxg ->
+                (let (nf, ng) :=
+                    square_bij_inv (S maxg) (square_bij (S maxg) (k1, k2)) in
+                    plus (vf nf) (vg ng))
+                = plus (vf k1) (vg k2)
+            ) as Eqsum.
+            move => k1 k2 Hk2.
+            rewrite is_bij_square.
+                2 : assumption.
+            reflexivity.
+        assert 
+            (∀ k1 k2 : nat, k2 <= maxg ->
+                scal
+                    (real (μ
+                    (λ x : X,
+                        square_bij (S maxg) (wf x, wg x) =
+                        square_bij (S maxg) (k1, k2))))
+                    (let (nf, ng) :=
+                    square_bij_inv (S maxg) (square_bij (S maxg) (k1, k2)) in
+                    plus (vf nf) (vg ng))
+                =
+                scal
+                    (real (μ (λ x : X, wf x = k1 ∧ wg x = k2)))
+                    (plus (vf k1) (vg k2))
+            ) as Eqsum1.
+            move => k1 k2 Hk2.
+            rewrite Eqsum. 2 : assumption.
+            rewrite Eqμ. 2 : assumption.
+            reflexivity.
+        clear Eqsum. clear Eqμ.
+        assert 
+            (∀ k1 : nat, 
+                sum_n
+                    (λ k2 : nat,
+                        scal
+                            (real (μ
+                            (λ x : X,
+                                square_bij (S maxg) (wf x, wg x) =
+                                square_bij (S maxg) (k1, k2))))
+                            (let (nf, ng) :=
+                            square_bij_inv (S maxg) (square_bij (S maxg) (k1, k2)) in
+                            plus (vf nf) (vg ng))
+                    ) maxg
+                =
+                sum_n
+                    (λ k2 : nat,
+                        scal
+                            (real (μ (λ x : X, wf x = k1 ∧ wg x = k2)))
+                            (plus (vf k1) (vg k2))
+                    ) maxg
+            ) as Eqsum.
+            move => k1.
+            apply sum_n_ext_loc => k2 Hk2.
+            apply Eqsum1 => //.
+        assert
+            (sum_n
+            (λ k1 : nat,
+                sum_n
+                    (λ k2 : nat,
+                        scal
+                        (real (μ
+                            (λ x : X,
+                            square_bij (S maxg) (wf x, wg x) =
+                            square_bij (S maxg) (k1, k2))))
+                        (let (nf, ng) :=
+                            square_bij_inv (S maxg) (square_bij (S maxg) (k1, k2)) in
+                            plus (vf nf) (vg ng))
+                    ) maxg
+                ) maxf
+            =
+            sum_n
+            (λ k1 : nat,
+                sum_n
+                    (λ k2 : nat,
+                        scal
+                        (real (μ (λ x : X, wf x = k1 ∧ wg x = k2)))
+                        (plus (vf k1) (vg k2))
+                    ) maxg
+                ) maxf
+            ) as Rwrt
+            by apply sum_n_ext => //.
+        rewrite Rwrt; clear Rwrt Eqsum Eqsum1.
+
     *)
-
-
+                
 End Bint_sf_plus.
